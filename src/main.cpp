e0632ce4b90f4acc8c3f57a658532039f1e807c1
bradynp <bpbraithwaite@gmail.com>
2023-01-18 13:10:37 -0700

    configured for multiple boards

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
+#include <dfm_mkr1310.h>
+#include <dfm_nanorp2040.h>
+
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
+#ifdef BUILD_MKR1310
+    setup_mkr1310();
+#endif
+#ifdef BUILD_NANORP2040
+    setup_nanorp2040();
+#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+#ifdef BUILD_MKR1310
+    loop_mkr1310();
+#endif
+#ifdef BUILD_NANORP2040
+    loop_nanorp2040();
+#endif
 }
