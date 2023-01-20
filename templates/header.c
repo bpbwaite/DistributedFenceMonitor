b7ad88251fbdf7ed043f8fd62145581eae366300
bradynp <bpbraithwaite@gmail.com>
2023-01-20 12:23:21 -0700

    initial commit

---
+++ templates/header.c
@@ -0,0 +1,8 @@
+/*
+  FILE: HEADER.C
+  VERSION: 0.0.1
+  DATE: 20 January 2023
+  PROJECT: Distributed Fence Monitor Capstone
+  AUTHORS: Briellyn Braithwaite
+  DESCRIPTION: An example header for all code files
+*/
