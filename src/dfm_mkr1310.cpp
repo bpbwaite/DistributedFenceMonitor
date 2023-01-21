0362260c6016b9bb7d475a60f027a0eb85cd97a3
bradynp <bpbraithwaite@gmail.com>
2023-01-20 18:25:19 -0700

    introduce secure element library

--- src/dfm_mkr1310.cpp
+++ src/dfm_mkr1310.cpp
@@ -11,12 +11,15 @@
 #ifdef BUILD_MKR1310
 
 #include <Arduino.h>
-#include <SPI.h>
+#include <ArduinoECCX08.h>
 #include <LoRa.h>
+#include <SPI.h>
 
-#define SERIALBAUD    115200
+#define SERIALBAUD 115200
+
 #define LORA_AMERICAN 915E6
 #define LORA_AFRICAN  868E6
+#define LORA_EUROPEAN 433E6
 
 int counter = 0;
 
@@ -33,6 +36,9 @@ void setup_mkr1310() {
     else {
         Serial.println("LoRa Module Online");
     }
+
+    LoRa.setGain(5);            // range 0-6
+    LoRa.setSpreadingFactor(7); // ranges from 6-12,default 7, sender/receiver must match
 }
 void loop_mkr1310() {
     LoRa.beginPacket();
