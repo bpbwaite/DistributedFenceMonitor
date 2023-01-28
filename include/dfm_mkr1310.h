a0f910513d9d9ec2440fcf6e1171b7b0bceeda26
bradynp <bpbraithwaite@gmail.com>
2023-01-27 22:56:57 -0700

    add defines for BW, SF, Syncword, preamble; remove SNR from struct

--- include/dfm_mkr1310.h
+++ include/dfm_mkr1310.h
@@ -19,10 +19,19 @@
 #define PIN_ERRORLED  3
 #define PIN_BATADC    A1
 
-#define SERIALBAUD   115200
-#define LORA_AMERICA 915E6
-#define LORA_AFRICA  868E6
-#define LORA_EUROPE  433E6
+#define SERIALBAUD      115200
+#define LORA_AMERICA    915E6
+#define LORA_AFRICA     868E6
+#define LORA_EUROPE     433E6
+#define SIGNALBANDWIDTH 125E3 // default 125E3. among other values, can also be 250E3 or 500E3.
+
+#define SPREADFACTOR 10
+// ranges from 6-12, default 7, sender/receiver must match.
+// higher not necessarily = further, but usually better strength (?)
+// The duration of a symbol is 2^SF / BW (SF: Spreading Factor, BW: Bandwidth)
+
+#define SYNCWORD    0x12 // default is 0x12, 0x34 is reserved for public communications, probably a byte
+#define PREAMBLELEN 8    // 6-65535, default 8
 
 typedef struct {
     uint16_t ID;
@@ -31,7 +40,6 @@ typedef struct {
     uint16_t connectedNodes;
     byte bat;
     byte freq;
-    float SNR;
     byte SF;
     byte SyncWord;
     uint32_t upTime;
