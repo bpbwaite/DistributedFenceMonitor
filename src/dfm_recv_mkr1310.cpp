3d158cfa65a2b8ec5a786e26e581a9a460ca17aa
bradynp <bpbraithwaite@gmail.com>
2023-02-08 20:45:21 -0700

    clean up string printing

--- src/dfm_recv_mkr1310.cpp
+++ src/dfm_recv_mkr1310.cpp
@@ -1,6 +1,6 @@
 /*
   FILE: DFM_MKR1310.CPP
-  VERSION: 0.1.4
+  VERSION: 0.1.5
   DATE: 8 February 2023
   PROJECT: Distributed Fence Monitor Capstone
   AUTHORS: Briellyn Braithwaite
@@ -23,51 +23,79 @@ void epchtostr(char *p, uint32_t epc) {
     struct tm ts;
     time_t now = (time_t) (epc + GMTOFFSET);
     ts         = *localtime(&now);
-    strftime(p, 20, "%m-%d-%Y %H:%M:%S", &ts);
+    strftime(p, 22, "%m-%d-%Y %H:%M:%S", &ts);
 }
 
-// pass a buffer of size 150 or greater please
 void mndtostr(char *p, const MonitoringNodeData d) {
+
+    char channelStr[20];
+    int k;
+    if (d.freq >= LoRaChannelsUS[0] && d.freq <= LoRaChannelsUS[NUMCHANNELS_US]) {
+        for (k = 0; k < NUMCHANNELS_US; ++k)
+            if (LoRaChannelsUS[k] == d.freq)
+                break;
+        sprintf(channelStr, "America Ch.%d", k);
+    }
+    else if (d.freq >= LoRaChannelsEU[0] && d.freq <= LoRaChannelsEU[NUMCHANNELS_EU]) {
+        int k;
+        for (k = 0; k < NUMCHANNELS_EU; ++k)
+            if (LoRaChannelsEU[k] == d.freq)
+                break;
+        sprintf(channelStr, "Europe Ch.%d", k);
+    }
+    else
+        strcpy(channelStr, "Unknown");
+
+    char syncWordGoodStr[4];
+    if (d.SyncWord == SYNCWORD)
+        strcpy(syncWordGoodStr, "OK");
+    else
+        strcpy(syncWordGoodStr, "ERR");
+
+    char epochStr[22];
+    epchtostr(epochStr, d.epoch);
+
+    char confStr[10];
+    sprintf(confStr, "SF%dBW%d", SPREADFACTOR, int(CHIRPBW / 1000U));
+
+    char rangeStr[6];
+    int rssi = LoRa.packetRssi();
+    int dist = 0;
+    if (rssi >= -18)
+        dist = 0;
+    else if (rssi >= -28)
+        dist = 1;
+    else if (rssi >= -48)
+        dist = 5;
+    else if (rssi >= -55)
+        dist = 10;
+    else if (rssi >= -60)
+        dist = 50;
+    else
+        dist = 5000;
+
+    sprintf(rangeStr, "~%d m", dist);
+
+    // snr relative strength is SF dependent
+    float snr = LoRa.packetSnr();
 
     PGM_P format = ">ID:     0x%02X\r\n"
                    ">Pack:   %d\r\n"
                    ">Stat:   0x%02X\r\n"
                    ">Cons:   %d\r\n"
                    ">Batt:   %d\r\n"
-                   ">Freq:   %02X (%s)\r\n"
-                   ">SF:     %d\r\n"
-                   ">SW:     0x%02X (%c)\r\n"
+                   ">Freq:   %.1fMHz (%s)\r\n"
+                   ">SW:     0x%04X (%s)\r\n"
                    ">Uptime: %ds\r\n"
                    ">TOA:    %dms\r\n"
-                   ">Temp:   %.1f C\r\n"
+                   ">Temp:   %.1fC\r\n"
                    ">Acc X:  %.2fg\r\n"
                    ">Acc Y:  %.2fg\r\n"
                    ">Acc Z:  %.2fg\r\n"
-                   ">Epoch:  %s";
-
-    char epochStr[20];
-    epchtostr(epochStr, d.epoch);
-
-    char localeStr[9];
-    switch (d.freq) {
-    case 0xAC:
-        strcpy(localeStr, "America");
-        break;
-    case 0xFA:
-        strcpy(localeStr, "Africa");
-        break;
-    case 0xEE:
-        strcpy(localeStr, "Europe");
-        break;
-    default:
-        break;
-    }
-
-    char syncWordIsGood[4];
-    if (d.SyncWord == SYNCWORD)
-        strcpy(syncWordIsGood, "OK");
-    else
-        strcpy(syncWordIsGood, "ERR");
+                   ">Epoch:  %s\r\n"
+                   ">Conf:   %s\r\n"
+                   ">RSSI:   %ddBmW (%s)\r\n"
+                   ">SNR:    %.1fdB";
 
     sprintf(p,
             format,
@@ -76,18 +104,21 @@ void mndtostr(char *p, const MonitoringNodeData d) {
             d.status,
             d.connectedNodes,
             d.bat,
-            d.freq,
-            localeStr,
-            SPREADFACTOR,
+            d.freq / 1000000.0,
+            channelStr,
             d.SyncWord,
-            syncWordIsGood,
+            syncWordGoodStr,
             d.upTime / 1000,
             d.timeOnAir,
             d.temperature,
             d.accelX,
             d.accelY,
             d.accelZ,
-            epochStr);
+            epochStr,
+            confStr,
+            rssi,
+            rangeStr,
+            snr);
 }
 void setup_recv_mkr1310() {
 
@@ -96,9 +127,10 @@ void setup_recv_mkr1310() {
         yield();
     Serial.println("Notice: Serial Interface Connected!");
 
-    long mode = LORA_AMERICA;
+    // long freq = LoRaChannelsUS[63];
+    long freq = 915E6;
 
-    if (!LoRa.begin(mode)) {
+    if (!LoRa.begin(freq)) {
         Serial.println("Error: LoRa Module Failure");
         while (1)
             ;
@@ -109,7 +141,7 @@ void setup_recv_mkr1310() {
 
     LoRa.setGain(RECEIVER_GAINMODE);
     LoRa.setSpreadingFactor(SPREADFACTOR);
-    LoRa.setSignalBandwidth(SIGNALBANDWIDTH);
+    LoRa.setSignalBandwidth(CHIRPBW);
     LoRa.setSyncWord(SYNCWORD);
     LoRa.setPreambleLength(PREAMBLELEN);
 
@@ -124,7 +156,7 @@ void setup_recv_mkr1310() {
     Serial.println("Notice: Central Node Setup Complete");
 }
 void loop_recv_mkr1310() {
-    static char serialOutBuffer[256];
+    char serialOutBuffer[512];
 
     int packetSize = LoRa.parsePacket();
     if (packetSize) {
@@ -139,13 +171,6 @@ void loop_recv_mkr1310() {
 
         mndtostr(serialOutBuffer, mndBuf);
         Serial.println(serialOutBuffer);
-
-        Serial.print("With RSSI of ");
-        Serial.print(LoRa.packetRssi());
-        Serial.println(" dBmW");
-        Serial.print("With SNR of ");
-        Serial.println(LoRa.packetSnr());
-        Serial.println();
     }
     delay(10);
 }
