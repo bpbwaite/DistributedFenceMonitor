0d2fcb3fee7590c29843d71dd4704accc1ac27e4
bradynp <bpbraithwaite@gmail.com>
2023-03-02 20:24:29 -0700

    initial commit

---
+++ src/interpreter/dummyPopulate.m
@@ -0,0 +1,25 @@
+clearvars
+
+databuf.packetnum = 1;
+databuf.id = 2;
+databuf.cons = 3;
+databuf.stat = 4;
+databuf.sw = 5;
+databuf.bat = 6;
+databuf.freq = 7;
+databuf.uptime = 8;
+databuf.toa = 9;
+databuf.temp = 1;
+databuf.acx = 2;
+databuf.acy = 3;
+databuf.acz = 4;
+databuf.epoch = 5;
+databuf.rssi = 6;
+databuf.snr = 7;
+databuf.bw = 8;
+databuf.sf = 9;
+
+
+alldata = struct([]);
+alldata = [alldata databuf]
+alldata = [alldata databuf]
