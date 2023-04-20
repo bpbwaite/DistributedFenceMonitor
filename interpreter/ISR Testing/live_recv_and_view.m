3d10dc085b0259310e8d186bd0bfd6a05a05f490
Briellyn Braithwaite <bpbraithwaite@gmail.com>
2023-04-19 20:23:44 -0700

    improved logic

--- interpreter/ISR Testing/live_recv_and_view.m
+++ interpreter/ISR Testing/live_recv_and_view.m
@@ -1,7 +1,7 @@
 %/*
 %  FILE: Live Viewer for accelerometer
-%  VERSION: 1.0.1
-%  DATE: 15 March 2023
+%  VERSION: 1.0.5
+%  DATE: 19 April 2023
 %  PROJECT: Distributed Fence Monitor Capstone
 %  AUTHORS: Briellyn Braithwaite
 %  DESCRIPTION: Streams 3-axis accelerometer data over serial
@@ -18,77 +18,97 @@ format compact
 baud = 115200; % bps
 timeout = 10; % s
 %% ADXL configuration
-% ( from datasheet )
-% SENS     LSB PER G
-%   2       256
-%   4       128
-%   8       64
-%   16      32
+
 lsb_per_g_xy = 265; % you read that correctly. assumes vcc = 3.3v
 lsb_per_g_z = 256;
 g = 9.81;
-adxl_sens = 2; % range in +- g
-adxl_adc_bits = 10; % 10
-BW = 50;
-Fs = 2*BW;
+
+Fs = 100;
 %% Connection
 spla = serialportlist';
 spla = spla(1);
 s = serialport(spla, 115200);
 s.Timeout = timeout;
-    
+
 disp("Setup complete!")
-disp("Receiving...")
+disp("Receiving")
 
 %% Ingest
-databuf.x = []; 
-databuf.y = []; 
-databuf.z = [];
+global databuf n keepback
+
 n = 1;
 
-t_window = 10; % seconds of data to show
-%keep_back = Fs * t_window;
+t_window = 5; % seconds of data to show
+keepback = t_window * Fs;
+
+databuf.x = zeros(1, keepback);
+databuf.y = zeros(1, keepback);
+databuf.z = zeros(1, keepback);
 
 figure(1), hold on
 grid on
 shg
-xlabel('t (s)')
-ylabel('Acceration (g)')
-h1 = plot(0,0,'r');
-h2 = plot(0,0,'g');
-h3 = plot(0,0,'b');
-ylim([-2.05 2.05])
-legend('X','Y','Z')
+xlabel('Time (sec)')
+ylabel('Acceration (m/s^2)')
+h1 = plot(0, 0, 'r', 'linewidth', 2);
+h2 = plot(0, 0, 'g', 'linewidth', 2);
+h3 = plot(0, 0, 'b', 'linewidth', 2);
+ylim([-20 20])
+
+xlower = 0 - 0.5;
+xupper = t_window + 0.5;
+xlim([xlower, xupper])
+t = 0:1 / Fs:t_window;
+t = t(1:end - 1);
+set(h1, 'XData', t)
+set(h2, 'XData', t)
+set(h3, 'XData', t)
+
+% show thresholds
+th_ms2 = sqrt(4.4294);
+plot([t(1) t(end)], [th_ms2 th_ms2], 'k--')
+plot([t(1) t(end)], [-th_ms2 -th_ms2], 'k--')
+
+legend('X Accel', 'Y Accel', 'Z Accel', '', '')
+
+configureCallback(s, "terminator", @handleSerialData)
+%%
+figure(1)
+fps = 24;
+fperinday = (1 / fps) / 60/60/24;
 
 while 1
-    while 1
-       c = read(s, 1, "uint8");
-       if c == 35 % start of a packet
-           break
-       end
+    completed_last = now;
+
+    while now - completed_last < fperinday
     end
-        databuf.x(n) = read(s, 1, "int32");
-        databuf.y(n) = read(s, 1, "int32");
-        databuf.z(n) = read(s, 1, "int32");
 
-        if ~mod(n, 800) % determines show freq
-            xlower = max(0, n/Fs - t_window);
-            xupper = max(t_window, n/Fs);
-            X = (1:length(databuf.x))./Fs;
-            
-            figure(1)
-            set(h1, 'XData', X, 'YData', databuf.x / lsb_per_g_xy);
-            set(h2, 'XData', X, 'YData', databuf.y / lsb_per_g_xy);
-            set(h3, 'XData', X, 'YData', databuf.z / lsb_per_g_z);
-            drawnow
-            xlim([xlower, xupper])
-        end
-        % terminator
-        newl = read(s, 2, "uint8");
-        
-        n = n+1;
-       
-        
+    set(h1, 'YData', databuf.x * g / lsb_per_g_xy);
+    set(h2, 'YData', databuf.y * g / lsb_per_g_xy);
+    set(h3, 'YData', databuf.z * g / lsb_per_g_z);
+
+    drawnow
+
 end
-%% CTRL-C to break from data collection
 
+%% Callback Function
+function handleSerialData(src, ~)
+    global databuf n keepback
+
+    if n == 1
+        src.flush
+        n = n + 1;
+        return
+    else
+
+        databuf.x(mod(n, keepback) + 1) = read(src, 1, "int32");
+        databuf.y(mod(n, keepback) + 1) = read(src, 1, "int32");
+        databuf.z(mod(n, keepback) + 1) = read(src, 1, "int32");
+
+        % extract terminator
+        read(src, 2, "uint8");
+
+        n = n + 1;
+    end
+
+end
